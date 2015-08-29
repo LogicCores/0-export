e7835efca57373ab2b5a6fe0642efe8116c3e11d
Christoph Dorn <christoph@christophdorn.com>
2015-08-28 20:29:48 -0700

    export dist

--- for/browserify/export.js
+++ for/browserify/export.js
@@ -8,28 +8,54 @@ exports.app = function (options) {
 
     return function (req, res, next) {
 
-        var path = PATH.join(options.basePath,  req.params[0]);
+        var path = PATH.join(options.distPath, req.params[0]);
 
-        return FS.exists(path, function (exists) {
-            if (!exists) return next();
+		return FS.exists(path, function (exists) {
 
-
-			var browserify = BROWSERIFY({
-				basedir: PATH.dirname(path)
-//				standalone: ''
-			});
-			browserify.add("./" + PATH.basename(path));
-
-			return browserify.bundle(function (err, data) {
-				if (err) return next(err);
-
-				data = require("../defs").transform(data);
+	        if (exists && /\.dist\./.test(path)) {
+	           	// We return a pre-built file if it exists and are being asked for it
 
 				res.writeHead(200, {
 					"Content-Type": "application/javascript"
 				});
-				return res.end(data);
-			});
+	           	return FS.createReadStream(path).pipe(res);
+
+	        } else {
+
+	           	// We build file, store it and return it
+
+	            path = PATH.join(options.basePath, req.params[0]).replace(/\.dist\./, ".");
+
+				return FS.exists(path, function (exists) {
+	
+		            if (!exists) return next();
+
+		            console.log("Browserifying '" + path + "' ...");
+
+					var browserify = BROWSERIFY({
+						basedir: PATH.dirname(path)
+		//				standalone: ''
+					});
+					browserify.add("./" + PATH.basename(path));
+		
+					return browserify.bundle(function (err, data) {
+						if (err) return next(err);
+		
+						data = require("../defs").transform(data);
+		
+				        var distPath = PATH.join(options.distPath, req.params[0]);
+				        
+				        return FS.writeFile(distPath, data, "utf8", function (err) {
+				        	if (err) return next(err);
+		
+							res.writeHead(200, {
+								"Content-Type": "application/javascript"
+							});
+							return res.end(data);
+				        });
+					});
+				});
+	        }
         });
     };
 }
