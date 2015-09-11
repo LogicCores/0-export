78c94c410743cf485ebdaecd592c93515fe37956
Christoph Dorn <christoph@christophdorn.com>
2015-09-10 22:24:40 -0700

    fixes for dynamic config and arbitrary mount/base paths

--- for/sm.hoist.VisualComponents/0-server.api.js
+++ for/sm.hoist.VisualComponents/0-server.api.js
@@ -1,111 +1,96 @@
 
 const PATH = require("path");
 const FS = require("fs");
+const URL = require("url");
 const ESCAPE_REGEXP_COMPONENT = require("escape-regexp-component");
-const EXPORT = require("./export");
 
+exports.forLib = function (LIB) {
 
-exports.app = function (options) {
+    const EXPORT = require("./export").forLib(LIB);
 
-    return function (req, res, next) {
+    var exports = {};
 
-        var uri = req.params[0];
+    exports.app = function (options) {
+    
+        return function (req, res, next) {
 
-        var htmRequested = /\.html?$/.test(uri);
-        if (!htmRequested) {
-            uri += ".htm" + (/\/index$/.test(uri) ? "l":"");
-        }
-        var baseUri = PATH.dirname(uri);
-        var filename = PATH.basename(uri);
+            var uri = req.params[0];
+            var htmRequested = /\.html?$/.test(uri);
 
-        // TODO: Refactor to 'cores/overlay'
-        function locateFile (baseUri, filename, callback) {
-            var path = PATH.join(options.basePath, baseUri, filename);
-            return FS.exists(path, function (exists) {
-                if (exists) {
-                    return callback(null, PATH.join(baseUri, filename));
+            return req.context.page.skinContextForUri(
+                uri
+            ).then(function (skinContext) {
+    
+                if (htmRequested) {
+    
+                    // The route was requested with the 'htm' extension so we serve
+                    // the raw file instead of the componentifed file.
+                    // TODO: Remove this option and return 404 in production.
+    
+                    // We convert the namespaced 'component' attributes as jQuery has a hard
+                    // time selcting attributes with colons in it. Likely true for many other
+                    // parsers as well.
+                    if (/;convert-to-data;/.test(req.headers["x-component-namespace"] || "")) {
+                        return FS.readFile(skinContext.skin.data.path, "utf8", function (err, html) {
+                            if (err) return next(err);
+                    
+    						var re = /(<|\s)component\s*:\s*([^=]+)(\s*=\s*"[^"]*"(?:\/?>|\s))/g;
+    						var m;
+    						while ( (m = re.exec(html)) ) {
+    							html = html.replace(
+    							    new RegExp(ESCAPE_REGEXP_COMPONENT(m[0]), "g"),
+    							    m[1] + "data-component-" + m[2].replace(/:/g, "-") + m[3]
+    							);
+    						}
+                            res.writeHead(200, {
+                                "Content-Type": "text/html"
+                            });
+                            res.end(html);
+                            return;
+                        });
+                    }
+    
+                    return FS.createReadStream(skinContext.skin.data.path).pipe(res);
                 }
-                // If the exact path is not found we check for an index file.
-                path = PATH.join(options.basePath, baseUri, "index.html");
-                return FS.exists(path, function (exists) {
-                    if (exists) {
-                        return callback(null, PATH.join(baseUri, "index.html"));
+
+                var baseUrlParts = URL.parse(skinContext.skin.host.baseUrl);
+                var configOverrides = {};
+                LIB._.merge(configOverrides, options.config);
+                LIB._.merge(configOverrides, {
+                    "source": {
+                        "server": {
+                            "host": baseUrlParts.host,
+                            "subPath": baseUrlParts.path
+                        }
                     }
-                    // The original path nor index file was found so we fall back
-                    // to the file of the parent secion and let the
-                    // client load the correct page content based on
-                    // window.location.pathname
-                    var parentBaseUri = PATH.dirname(baseUri);
-                    if (parentBaseUri === baseUri) {
-                        var err = new Error("No file found for uri: " + uri);
-                        err.code = 404;
-                        return callback(err);
-                    }
-                    return locateFile(parentBaseUri, filename, callback);
                 });
-            });
-        }
 
-        return locateFile(baseUri, filename, function (err, uri) {
-            if (err) return next(err);
-            
-            var path = PATH.join(options.basePath, uri);
-
-            if (htmRequested) {
-
-                // The route was requested with the 'htm' extension so we serve
-                // the raw file instead of the componentifed file.
-                // TODO: Remove this option and return 404 in production.
-
-                // We convert the namespaced 'component' attributes as jQuery has a hard
-                // time selcting attributes with colons in it. Likely true for many other
-                // parsers as well.
-                if (/;convert-to-data;/.test(req.headers["x-component-namespace"] || "")) {
-                    return FS.readFile(path, "utf8", function (err, html) {
+                return EXPORT.parseForUri(
+                    skinContext.skin.data.componentsPath,
+                    skinContext.skin.host.path,
+                    configOverrides,
+                    function (err, manifest) {
                         if (err) return next(err);
-                
-						var re = /(<|\s)component\s*:\s*([^=]+)(\s*=\s*"[^"]*"(?:\/?>|\s))/g;
-						var m;
-						while ( (m = re.exec(html)) ) {
-							html = html.replace(
-							    new RegExp(ESCAPE_REGEXP_COMPONENT(m[0]), "g"),
-							    m[1] + "data-component-" + m[2].replace(/:/g, "-") + m[3]
-							);
-						}
+
+                        if (
+                            !manifest.components ||
+                            !manifest.components.html ||
+                            !manifest.components.html.fsHtmlPath
+                        ) {
+                            var err = new Error("'html' component not declared in exports for uri '" + uri + "'!");
+                            err.code = 404;
+                            return next(err);
+                        }
+    
                         res.writeHead(200, {
                             "Content-Type": "text/html"
                         });
-                        res.end(html);
-                        return;
-                    });
-                }
-
-                return FS.createReadStream(path).pipe(res);
-            }
-
-            return EXPORT.parseForUri(
-                PATH.join(options.basePath, "components.json"),
-                uri,
-                function (err, manifest) {
-                    if (err) return next(err);
-                    
-                    if (
-                        !manifest.components ||
-                        !manifest.components.html ||
-                        !manifest.components.html.fsHtmlPath
-                    ) {
-                        var err = new Error("'html' component not declared in exports for uri '" + uri + "'!");
-                        err.code = 404;
-                        return next(err);
+                        return FS.createReadStream(manifest.components.html.fsHtmlPath).pipe(res);
                     }
+                );
+            }).catch(next);
+        };
+    }
 
-                    res.writeHead(200, {
-                        "Content-Type": "text/html"
-                    });
-                    return FS.createReadStream(manifest.components.html.fsHtmlPath).pipe(res);
-                }
-            );
-        });
-    };
+    return exports;
 }
-
--- for/sm.hoist.VisualComponents/export.js
+++ for/sm.hoist.VisualComponents/export.js
@@ -4,131 +4,141 @@ const FS = require("fs");
 const SPAWN = require("child_process").spawn;
 const SM_HOIST_COMMAND_PATH = require.resolve("../../../../lib/sm.hoist.VisualComponents/sm.hoist");
 
+exports.forLib = function (LIB) {
 
-exports.parseForUri = function (configPath, uri, callback) {
-    
-    try {
+    var exports = {};
 
-        return FS.readFile(configPath, "utf8", function (err, config) {
-            if (err) return callback(err);
+    exports.parseForUri = function (configPath, uri, configOverrides, callback) {
+        
+        try {
+    
+            return FS.readFile(configPath, "utf8", function (err, config) {
+                if (err) return callback(err);
+    
+                config = config.replace(/\{\{__DIRNAME__\}\}/g, PATH.dirname(configPath));
+                config = config.replace(/\{\{env\.PORT\}\}/g, process.env.PORT);
+                config = JSON.parse(config).config;
+    
+                var hoistConfig = {};
+                LIB._.merge(hoistConfig, config["sm.hoist.visualcomponents/0"]);
+                LIB._.merge(hoistConfig, configOverrides);
 
-            config = config.replace(/\{\{__DIRNAME__\}\}/g, PATH.dirname(configPath));
-            config = config.replace(/\{\{env\.PORT\}\}/g, process.env.PORT);
-            config = JSON.parse(config).config;
 
-        	var manifestPath = PATH.join(config['sm.hoist.visualcomponents/0'].target.path, "hoisted.json");
-            var pageUri = uri.replace(/(^\/|\.html?)/g, "");
+            	var manifestPath = PATH.join(hoistConfig.target.path, "hoisted.json");
+                var pageUri = uri.replace(/(^\/|\.html?)/g, "");
+            
+                // TODO: Add proper cache management instead of just checking if it exists.
+            	return FS.exists(manifestPath, function (exists) {
+            
+            	    function returnPageManifest (callback) {
+            
+                        console.log("manifestPath", manifestPath);
         
-            // TODO: Add proper cache management instead of just checking if it exists.
-        	return FS.exists(manifestPath, function (exists) {
-        
-        	    function returnPageManifest (callback) {
-        
-                    console.log("manifestPath", manifestPath);
+            	        return FS.readFile(manifestPath, "utf8", function (err, data) {
+            
+                            console.log("data", data);	            
+            	            
+                            var manifest = JSON.parse(data);
+                            var page = manifest.pages[pageUri];
+                            if (!page) {
+                                return callback(new Error("No components found in manifest '" + manifestPath + "' for uri '" + pageUri + "'!"));
+                            }
+            
+                            // TODO: Use config adapter to instanciate config.
+                            page = JSON.parse(JSON.stringify(page).replace(
+                                /\{\{__DIRNAME__\}\}/g,
+                                PATH.dirname(manifestPath)
+                            ));
+            
+                            return callback(null, page);
+                        });
+            	    }
+            	    
+            	    if (exists) {
+            //	        return returnPageManifest(callback);
+            	    }
+            
+            	    function runOutOfProcess () {
+            
+                        console.log("Hoist out of process:", uri);
+            
+                	    var proc = SPAWN(SM_HOIST_COMMAND_PATH, [
+                            configPath,
+                            "--build",
+                            "--uri", uri
+                        ], {
+                        	env: process.env
+                        });
+                        proc.on("error", function(err) {
+                        	return callback(err);
+                        });
+                        var stdout = [];
+                        var stderr = [];
+                        proc.stdout.on('data', function (data) {
+                        	stdout.push(data.toString());
+                    		process.stdout.write(data);
+                        });
+                        proc.stderr.on('data', function (data) {
+                        	stderr.push(data.toString());
+                    		process.stderr.write(data);
+                        });
+                        proc.on('close', function (code) {
+                        	if (code) {
+                        		var err = new Error("sm.hoist exited with code: " + code);
+                        		err.code = code;
+                        		err.stdout = stdout;
+                        		err.stderr = stderr;
+                        		return callback(err);
+                        	}
+                	        return returnPageManifest(callback);
+                        });
+            	    }
+            
+            	    function runInProcess (callback) {
+            
+                        console.log("Hoist in process:", uri);
+            
+            	        const EXPORT = require("../../../../lib/sm.hoist.VisualComponents/lib/export").forLib(
+            	            require("../../../../lib/sm.hoist.VisualComponents/lib/lib")
+            	        );
     
-        	        return FS.readFile(manifestPath, "utf8", function (err, data) {
+            	        return EXPORT.export({
+            	            build: true,
+            	            uri: uri,
+            	            config: hoistConfig
+            	        }).then(function (descriptor) {
+            	            
+                            // TODO: Use config adapter to instanciate config.
+                            descriptor = JSON.parse(JSON.stringify(descriptor).replace(
+                                /\{\{__DIRNAME__\}\}/g,
+                                PATH.dirname(manifestPath)
+                            ));
         
-                        console.log("data", data);	            
-        	            
-                        var manifest = JSON.parse(data);
-                        var page = manifest.pages[pageUri];
-                        if (!page) {
-                            return callback(new Error("No components found in manifest '" + manifestPath + "' for uri '" + pageUri + "'!"));
-                        }
+                            var page = descriptor.pages[pageUri];
+                            if (!page) {
+                                return callback(new Error("No components found in manifest '" + manifestPath + "' for uri '" + pageUri + "'!"));
+                            }
         
-                        // TODO: Use config adapter to instanciate config.
-                        page = JSON.parse(JSON.stringify(page).replace(
-                            /\{\{__DIRNAME__\}\}/g,
-                            PATH.dirname(manifestPath)
-                        ));
-        
-                        return callback(null, page);
-                    });
-        	    }
-        	    
-        	    if (exists) {
-        //	        return returnPageManifest(callback);
-        	    }
-        
-        	    function runOutOfProcess () {
-        
-                    console.log("Hoist out of process:", uri);
-        
-            	    var proc = SPAWN(SM_HOIST_COMMAND_PATH, [
-                        configPath,
-                        "--build",
-                        "--uri", uri
-                    ], {
-                    	env: process.env
-                    });
-                    proc.on("error", function(err) {
-                    	return callback(err);
-                    });
-                    var stdout = [];
-                    var stderr = [];
-                    proc.stdout.on('data', function (data) {
-                    	stdout.push(data.toString());
-                		process.stdout.write(data);
-                    });
-                    proc.stderr.on('data', function (data) {
-                    	stderr.push(data.toString());
-                		process.stderr.write(data);
-                    });
-                    proc.on('close', function (code) {
-                    	if (code) {
-                    		var err = new Error("sm.hoist exited with code: " + code);
-                    		err.code = code;
-                    		err.stdout = stdout;
-                    		err.stderr = stderr;
-                    		return callback(err);
-                    	}
-            	        return returnPageManifest(callback);
-                    });
-        	    }
-        
-        	    function runInProcess (callback) {
-        
-                    console.log("Hoist in process:", uri);
-        
-        	        const EXPORT = require("../../../../lib/sm.hoist.VisualComponents/lib/export").forLib(
-        	            require("../../../../lib/sm.hoist.VisualComponents/lib/lib")
-        	        );
-        
-    
-        	        return EXPORT.export({
-        	            build: true,
-        	            uri: uri,
-        	            config: config["sm.hoist.visualcomponents/0"]
-        	        }).then(function (descriptor) {
-        	            
-                        // TODO: Use config adapter to instanciate config.
-                        descriptor = JSON.parse(JSON.stringify(descriptor).replace(
-                            /\{\{__DIRNAME__\}\}/g,
-                            PATH.dirname(manifestPath)
-                        ));
-    
-                        var page = descriptor.pages[pageUri];
-                        if (!page) {
-                            return callback(new Error("No components found in manifest '" + manifestPath + "' for uri '" + pageUri + "'!"));
-                        }
-    
-                        return callback(null, page);
-        	        }, callback);
-        	    }
-        
-        	    // If we are in NodeJS 4 we can run it in process.
-        	    // Otherwise we have to run it out of process as
-        	    // iojs/nodejs >= 4 is required by jsdom used by 'sm.hoist.VisualComponents'.
-        	    console.log("NodeJS version:", process.version);
-        	    if ( parseInt(process.version.replace(/^v/, "").split(".").shift()) >= 4 ) {
-        	        return runInProcess(callback);
-        	    } else {
-            	    return runOutOfProcess(callback);
-        	    }
+                            return callback(null, page);
+            	        }, callback);
+            	    }
+            
+            	    // If we are in NodeJS 4 we can run it in process.
+            	    // Otherwise we have to run it out of process as
+            	    // iojs/nodejs >= 4 is required by jsdom used by 'sm.hoist.VisualComponents'.
+            	    console.log("NodeJS version:", process.version);
+            	    if ( parseInt(process.version.replace(/^v/, "").split(".").shift()) >= 4 ) {
+            	        return runInProcess(callback);
+            	    } else {
+            	        return callback(new Error("TODO: Fix out-of-process rendering using injected config"));
+                	    //return runOutOfProcess(callback);
+            	    }
+            	});
         	});
-    	});
-    } catch (err) {
-        return callback(err);
+        } catch (err) {
+            return callback(err);
+        }
     }
+    
+    return exports;
 }
-
