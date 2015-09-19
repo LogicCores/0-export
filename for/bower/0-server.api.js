678ca8e9137e9a9eec6ee345e2353a6d97baef5b
Christoph Dorn <christoph@christophdorn.com>
2015-09-18 22:27:30 -0700

    bower

---
+++ for/bower/0-server.api.js
@@ -0,0 +1,86 @@
+
+exports.forLib = function (LIB) {
+
+    const GRUNT = require("grunt");
+
+	var exports = {};
+
+
+    var currentlyConcatenating = {};
+    exports.concatenate = function (componentsPath, distPath, config) {
+        if (currentlyConcatenating[componentsPath]) {
+            return currentlyConcatenating[componentsPath];
+        }
+        return (currentlyConcatenating[componentsPath] = LIB.Promise.promisify(function (callback) {
+            
+            console.log("Concatenating bower files from '" + componentsPath + "' ...");
+
+            // TODO: Use our own grunt instance instead of global instance.
+			GRUNT.file.setBase(componentsPath);
+
+			var gruntConfig = {
+                bower_concat: {
+                    all: {
+                        dest: distPath.replace(/\.[^\.]+$/, ".js"),
+					    cssDest: distPath.replace(/\.[^\.]+$/, ".css"),
+					    exclude: config.exclude || [],
+					    dependencies: config.dependencies || {},
+					    bowerOptions: {
+					      relative: false
+					    }
+                    }
+                }				                
+            };
+
+            //console.log("gruntConfig", JSON.stringify(gruntConfig, null, 4));
+
+			GRUNT.initConfig(gruntConfig);
+            GRUNT.loadTasks(LIB.path.dirname(require.resolve("grunt-bower-concat/package.json")) + "/tasks");
+            GRUNT.registerInitTask('default', function() {
+                GRUNT.task.run([
+                	"bower_concat"
+                ]);
+            });
+
+            return GRUNT.tasks(['default'], {
+                debug: false,
+                verbose: true
+            }, callback);
+        })()).then(function () {
+            delete currentlyConcatenating[componentsPath];
+        });
+    }
+
+
+	exports.app = function (options) {
+	
+	    return function (req, res, next) {
+
+	        var componentsPath = options.packagePath;
+	        var distPath = LIB.path.join(options.distPath, "bundle." + req.params[1]);
+
+            function serve () {
+    			res.writeHead(200, {
+    				"Content-Type": (req.params[1] === "js") ?
+    				    "application/javascript" :
+    				    "text/css"
+    			});
+               	return LIB.fs.createReadStream(distPath).pipe(res);
+            }
+
+			return LIB.fs.exists(distPath, function (exists) {
+		        if (exists && req.params[0]) {
+		           	// We return a pre-built file if it exists and are being asked for it
+		           	return serve();
+		        } else {
+		           	// We build file, store it and return it
+                    return exports.concatenate(componentsPath, distPath, options.config).then(function () {
+    		           	return serve();
+		           	}).catch(next);
+		        }
+	        });
+	    };
+	}
+
+	return exports;
+}
---
+++ for/bower/0-server.ccjson.js
@@ -0,0 +1,37 @@
+
+exports.forLib = function (LIB) {
+    var ccjson = this;
+
+    const SERVER = require("./0-server.api").forLib(LIB);
+
+    return LIB.Promise.resolve({
+        forConfig: function (defaultConfig) {
+
+            var Entity = function (instanceConfig) {
+                var self = this;
+
+                self.AspectInstance = function (aspectConfig) {
+
+                    var config = {};
+                    LIB._.merge(config, defaultConfig);
+                    LIB._.merge(config, instanceConfig);
+                    LIB._.merge(config, aspectConfig);
+
+                    return LIB.Promise.resolve({
+                        app: function () {
+
+                            return LIB.Promise.resolve(
+                                ccjson.makeDetachedFunction(
+                                    SERVER.app(config)
+                                )
+                            );
+                        }
+                    });
+                }
+            }
+            Entity.prototype.config = defaultConfig;
+
+            return Entity;
+        }
+    });
+}
